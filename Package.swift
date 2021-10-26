47353f3749f6ae9faca508bd56c10d55a4e809fd
svc-teamcity-agent <svc-teamcity-agent@svc-teamsity-agent.local>
2021-10-26 12:53:40 +0300

    Release 1.0.21

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapsSDK",
-            url: "https://mmr.jfrog.io/artifactory/maps-sdk-ios/1.0.20/MapsSDK.xcframework.zip",
-            checksum: "933315e753f1775ef1c39f08ce868a93dcd238b46f9c1a3b2be6063dc488a19d"
+            url: "https://mmr.jfrog.io/artifactory/maps-sdk-ios/1.0.21/MapsSDK.xcframework.zip",
+            checksum: "62282ba063d189252e91f2d3d7a812483260d3c8e98649f488d3b52b660bf531"
         )
     ]
 )
