d840ac44b4fd8b8adf0e27e239f249b6a0d47378
Teamcity <teamcity@dev-autotest-b-16d2.local>
2023-02-02 14:01:09 +0300

    Release 1.0.111

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapsSDK",
-            url: "https://mmr.jfrog.io/artifactory/maps-sdk-ios/1.0.110/MapsSDK.xcframework.zip",
-            checksum: "12bbb3c5b8188450f63ebbec88997b4a0df8d52c8d361445dd13c164025739ec"
+            url: "https://mmr.jfrog.io/artifactory/maps-sdk-ios/1.0.111/MapsSDK.xcframework.zip",
+            checksum: "bdd7ee017e3cc19f921b5d7cfeda16fde4ff8687a78518a1e06171cc106ccc79"
         )
     ]
 )
