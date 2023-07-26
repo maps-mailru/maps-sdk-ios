2d612ea5c7e6535a61a82a0f3bd551c22c52bc7c
mapsagent <mapsagent@vk.team>
2023-07-26 12:35:17 +0300

    Release 1.0.143

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapsSDK",
-            url: "https://artifactory-external.vkpartner.ru/artifactory/maps-sdk-ios/1.0.142/MapsSDK.xcframework.zip",
-            checksum: "01a3a2156fd072f0498e4077d8490b18da8087e99869266bdc5a82b1bbcc5f9f"
+            url: "https://artifactory-external.vkpartner.ru/artifactory/maps-sdk-ios/1.0.143/MapsSDK.xcframework.zip",
+            checksum: "15bde7fa8a4f42d17b3dd8d0a2ef8bacd2f2a6c535e9e7f47cad8b8c6c2623ee"
         )
     ]
 )
